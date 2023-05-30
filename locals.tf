7481b4b5a381bfd0a12c1e7114e08849d82d646f
Osman Mohammed <osman.mohammed@camptocamp.com>
2023-05-30 17:20:53 +0200

    feat: enable ingress

--- locals.tf
+++ locals.tf
@@ -1,3 +1,28 @@
 locals {
-  helm_values = [{}]
+  helm_values = [{
+    allInOne = {
+      ingress = {
+        enabled = true
+        annotations = {
+          "cert-manager.io/cluster-issuer"                   = "${var.cluster_issuer}"
+          "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+          "traefik.ingress.kubernetes.io/router.middlewares" = "traefik-withclustername@kubernetescrd"
+          "traefik.ingress.kubernetes.io/router.tls"         = "true"
+          "ingress.kubernetes.io/ssl-redirect"               = "true"
+          "kubernetes.io/ingress.allow-http"                 = "false"
+        }
+        hosts = [
+          "jaeger.apps.${var.base_domain}",
+          "jaeger.apps.${var.cluster_name}.${var.base_domain}",
+        ]
+        tls = [{
+          secretName = "jaeger-tls"
+          hosts = [
+            "jaeger.apps.${var.base_domain}",
+            "jaeger.apps.${var.cluster_name}.${var.base_domain}",
+          ]
+        }]
+      }
+    }
+  }]
 }
