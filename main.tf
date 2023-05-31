d86e13891ac8c8b1a4d068c00558a54f0bf2baee
Osman Mohammed <osman.mohammed@camptocamp.com>
2023-05-31 09:31:09 +0200

    fix: correct path helm chart

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "argocd_application" "this" {
 
     source {
       repo_url        = "https://github.com/omohammed/devops-stack-module-jaeger.git"
-      path            = "charts/jaeger"
+      path            = "charts/jaegertracing"
       target_revision = var.target_revision
       helm {
         values = data.utils_deep_merge_yaml.values.output
