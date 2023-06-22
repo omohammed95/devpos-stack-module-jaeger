7c2ab85bc1b89e5e71326d7235a4d1f02446d27d
Mohammed Osman <53604473+omohammed95@users.noreply.github.com>
2023-06-22 22:44:50 +0200

    Update chart_bumper.py

--- charts/jaegertracing/chart_bumper.py
+++ charts/jaegertracing/chart_bumper.py
@@ -1,13 +1,14 @@
+
+"""
+Simple python script that is used in GitHub Actions to
+automatically bump chart dependencies using the updatecli CLI tool.
+"""
+
 from pathlib import Path
 import subprocess
 import yaml
 import os
-
-# Get the absolute path of the current script
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Specify the path to the chart relative to the current script directory
-path_chart = os.path.join(current_dir, "charts/jaegertracing")
+import traceback
 
 # Add charts here where it is known that higher versions are not
 # yet stable or that you would like to disable automatic upgrades for
@@ -17,14 +18,22 @@ EXCLUDED_CHARTS = []
 # bump major chart versions too. Make sure you inspect the upgrade instructions before merging!
 BUMP_MAJOR = os.environ.get("BUMP_MAJOR") == "true"
 
+# Path to the chart directory
+CHART_PATH = "charts/jaegertracing"
 
 def update_chart(path_chart: str):
     """
-    Given a path to a helm chart, bump the version of the dependencies of this chart
+    Given a path to a helm chart. Bump the version of the dependencies of this chart
     if any newer versions exist.
     """
 
-    with open(path_chart + "/Chart.yaml") as f:
+    chart_file = os.path.join(path_chart, "Chart.yaml")
+    
+    if not os.path.isfile(chart_file):
+        print(f"Chart.yaml not found in {path_chart}")
+        return
+
+    with open(chart_file) as f:
         text = f.read()
 
     chart: dict = yaml.safe_load(text)
@@ -38,40 +47,35 @@ def update_chart(path_chart: str):
             print(f"Skipping {dependency['name']} because it is excluded..")
             continue
 
-        # Bump major or minor depending on the set env variable
+        # bump major or minor depending on set env variable
         version = f"{dependency['version'].split('.')[0]}.*.*" if not BUMP_MAJOR else "*.*.*"
         manifest = f"""
 sources:
-  latestMinorRelease:
-    kind: helmChart
-    spec:
-      url: "{dependency["repository"]}"
-      name: "{dependency["name"]}"
-      version: "{version}"
+    latestMinorRelease:
+        kind: helmChart
+        spec:
+            url: "{dependency['repository']}"
+            name: "{dependency['name']}"
+            version: "{version}"
 conditions: {{}}
 targets:
-  chart:
-    name: "Bump Chart dependencies"
-    kind: helmChart
-    spec:
-      name: "{path_chart}"
-      file: "Chart.yaml"
-      key: "dependencies[{i}].version"
-      versionIncrement: "patch"
+    chart:
+        name: Bump Chart dependencies
+        kind: helmChart
+        spec:
+            Name: "{path_chart}"
+            file: "Chart.yaml"
+            key: "dependencies[{i}].version"
+            versionIncrement: "patch"
 """
 
-        with open("manifest.yaml", "w") as f:
+        with open(os.path.join(path_chart, "manifest.yaml"), "w") as f:
             f.write(manifest)
 
-        subprocess.run(["updatecli", "apply", "--config", "manifest.yaml"])
-
-
-def main():
-    # Modify the path_chart variable to point to the directory where your charts are located
-    path_chart = "charts/jaegertracing"
-
-    update_chart(path_chart)
-
+        subprocess.check_output("updatecli apply --config charts/jaegertracing/manifest.yaml".split(" "))
 
 if __name__ == "__main__":
-    main()
+
+    # Update the chart path with the actual path to your chart
+    path_chart = CHART_PATH
+    update_chart(path_chart)
